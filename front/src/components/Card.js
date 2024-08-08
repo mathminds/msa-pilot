b021dffaa8374f3b56fa605a652cdfd97e28efb4
Christopher Lee <mathmind@Christophers-MacBook.local>
2024-08-08 10:44:08 +0900

    initial skeleton

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -5,9 +5,9 @@ const Card = ({ title, count, details, button1, button2 }) => {
     return (
         <div className="card p-4 rounded-lg mb-4 border-2 mx-4 items-center flex-col">
             <h3 className="my-4 lg:text-xl font-bold text-black md:text-lg sm:text-xs">
-                {title}
+                {title} 
             </h3>
-            <div className='flex lg:flex-row md:flex-col sm:flex-col space-around h-24  w-full object-cover md:h-full md:w-48 md:text-4xl'>
+            <div className='flex lg:flex-row md:flex-col sm:flex-col space-around h-24  w-full object-cover'>
 
 
                 <div className="text-4xl font-bold bg-white flex flex-row items-center rounded-xl justify-center px-4 sm:py-8 sm:text-5xl mb-4 text-black sm:mx-1 sm:rounded-xl md:rounded-2xl lg:rounded-3xl" >
@@ -17,7 +17,7 @@ const Card = ({ title, count, details, button1, button2 }) => {
                     </div>
                 </div>
 
-                <div className="lg:flex-col md:flex-row sm:flex-col md:ml-0 sm:ml-0 sm:mx-2 md:justify-center pl-8" >
+                <div className="lg:flex-col md:flex-row sm:flex-col md:ml-0 sm:ml-0 sm:mx-2  pl-8" >
                     <a href={button1}>
                         <button className="text-xs md:px-2 md:py-1 border-none bg-orange-300 text-black cursor-pointer rounded sm:mx-0 md:mx-2 sm:text-2xs">
                             목록보기
