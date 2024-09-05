d67bb69ccf0cf4b6ec43b184950d7911a72c13d9
Chris Lee <mathmind@protonmail.com>
2024-09-05 14:07:51 +0900

    mobile version

--- front/src/components/serviceComponents/FinalServiceCard.js
+++ front/src/components/serviceComponents/FinalServiceCard.js
@@ -70,20 +70,20 @@ const FinalServiceCard = (props) => {
                         <li className='text-sm md:text-xl pt-1 row-start-5 row-end-7 col-start-1 col-end-13 overflow-hidden whitespace-nowrap overflow-ellipsis'>활용데이터: {dataProvidedList}</li>
                         <li className='text-sm md:text-xl pt-1 row-start-7 row-end-9 col-start-1 col-end-13 overflow-hidden whitespace-nowrap overflow-ellipsis'>정보전송자: {dataProvidersList}</li>
                         </ul>
-                        <div className='py-2 border-black bg-white w-full col-start-1 col-end-13 row-start-9 row-end-13 flex justify-center md:justify-between items-end'>
+                        <div className='py-2 border-black bg-white w-full col-start-1 col-end-13 row-start-9 row-end-13 flex justify-center gap-1 md:justify-between items-end'>
                             {/* <div className='grid grid-cols-12 grid-rows-2'> */}
-                                <div className='bg-blue-500 text-white font-bold btn btn-xs md:btn-sm col-start-1 col-end-5' onClick={handleOpenModal}>
+                                <div className='bg-blue-500 text-white font-bold btn btn-xs px-1.5 md:btn-sm col-start-1 col-end-5' onClick={handleOpenModal}>
                                     서비스 상세보기
                                 </div>
-                                <div className='bg-red-500 text-white font-bold btn btn-xs md:btn-sm col-start-5 col-end-9' onClick={handleOpenRejectModal}>
+                                <div className='bg-red-500 text-white font-bold btn btn-xs px-1.5 md:btn-sm col-start-5 col-end-9' onClick={handleOpenRejectModal}>
                                     서비스 철회하기
                                 </div>
                                 {thirdPartyRecipients.length > 0 ? 
-                                <div className='bg-yellow-500 text-white font-bold btn btn-xs md:btn-sm col-start-9 col-end-13' onClick={handleFlip}>
+                                <div className='bg-yellow-500 text-white font-bold btn btn-xs px-1.5 md:btn-sm col-start-9 col-end-13' onClick={handleFlip}>
                                     제3자 제공내역
                                 </div>
                                 :
-                                <div className='bg-gray-200 text-white font-bold btn btn-xs md:btn-sm col-start-9 col-end-13'>
+                                <div className='bg-gray-200 text-white font-bold btn btn-xs px-1 md:btn-sm col-start-9 col-end-13'>
                                     제3자 제공내역
                                 </div>
 }
@@ -93,7 +93,7 @@ const FinalServiceCard = (props) => {
                 </div>
                 <div className="flip-card-back md:flip-card-back-md grid grid-cols-12 grid-rows-12 text-black" onClick={handleFlip}>
                 <div className="consent-card h-[150px] w-[300px] md:h-[200px] md:w-[400px] col-start-2 col-end-12">
-      <div className="text-lg md:text-2xl consent-header row-start-1 row-end-2 pb-1 md: pb-2">
+      <div className="text-lg md:text-2xl consent-header row-start-1 row-end-2 pb-1 md:pb-2">
         [동의 일자] 2024년 8월 13일 18:32
       </div>
       <div className="consent-table">
